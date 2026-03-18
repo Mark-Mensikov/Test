a43b4a570e89c040266da9af20222a3a1393e35b
Mark Menšikov <131643380+Mark-Mensikov@users.noreply.github.com>
2026-03-18 12:52:53 +0200

    Initialize Express server with static file serving

---
+++ front/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = parseInt(process.env.PORT, 10) || 3000;
+const PB_URL = 'http://pocketbase-hv4bvovfn86uwdafxs7d7p1s.176.112.158.3.sslip.io';
+
+app.use(express.static(__dirname));
+
+app.get('/config', (req, res) => {
+    res.json({ pbUrl: PB_URL });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
