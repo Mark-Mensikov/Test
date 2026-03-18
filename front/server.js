c060efe87958a650ddfbbcd56eaf15eb0887a96b
Mark Menšikov <131643380+Mark-Mensikov@users.noreply.github.com>
2026-03-18 13:09:12 +0200

    Update server.js

--- front/server.js
+++ front/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 
 const PORT = parseInt(process.env.PORT, 10) || 3000;
+// Убедитесь, что этот URL ведет на ваш PocketBase в Coolify
 const PB_URL = 'http://pocketbase-hv4bvovfn86uwdafxs7d7p1s.176.112.158.3.sslip.io';
 
 app.use(express.static(__dirname));
